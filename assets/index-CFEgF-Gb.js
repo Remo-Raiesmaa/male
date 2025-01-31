03a00f3058b0ca2611adb22e2e0b74889e7703fb
doktorjohan <johan.hollak@ut.ee>
2025-01-31 14:29:05 +0200

    Updates

---
+++ assets/index-CFEgF-Gb.js
@@ -0,0 +1,1 @@
+import{c as e,o as s}from"./index-hYKudiXO.js";const r={__name:"index",setup(a){return(n,t)=>(s(),e("p",null,"siin on esileht. selle arendamise jätame viimaseks"))}};export{r as default};
