f1036658498a863d6ec641a1ebb392b8fbd498cd
doktorjohan <johan.hollak@ut.ee>
2025-01-31 13:42:06 +0200

    Updates

---
+++ assets/index-BG-VAyqV.js
@@ -0,0 +1,1 @@
+import{c as e,o as s}from"./index-L4n5l3BN.js";const r={__name:"index",setup(a){return(n,t)=>(s(),e("p",null,"siin on esileht. selle arendamise jätame viimaseks"))}};export{r as default};
---
+++ assets/login--psXWQ0p.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./index-L4n5l3BN.js";const r={};function c(e,t){return null}const o=n(r,[["render",c]]);export{o as default};
