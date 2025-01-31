4c410f1f124d6b35b6c9b9d3ab14ff8d397f0e13
doktorjohan <johan.hollak@ut.ee>
2025-01-31 14:56:18 +0200

    Updates

---
+++ assets/index-COdX3Dc2.js
@@ -0,0 +1,1 @@
+import{c as e,o as s}from"./index-D8rOt4Tv.js";const r={__name:"index",setup(a){return(n,t)=>(s(),e("p",null,"siin on esileht. selle arendamise jätame viimaseks"))}};export{r as default};
